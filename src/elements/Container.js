dd4f4cfc21cd6e180e0ef9d3d0b84ea5a6e855e7
Karcub <rokaszemumenyecske@gmail.com>
2021-09-08 13:22:08 +0200

    feat: added container for elements

---
+++ src/elements/Container.js
@@ -0,0 +1,14 @@
+
+import "../styles/situation.css";
+
+const Container = () => {
+  return (
+      <>
+          <div className="container" id="cont">
+
+          </div>
+      </>
+  )
+}
+
+export default Container;
