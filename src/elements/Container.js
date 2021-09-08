e75b1593002e4a927faffb934a262e78f0208e63
Karcub <rokaszemumenyecske@gmail.com>
2021-09-08 13:26:57 +0200

    feat: added situation element to container

--- src/elements/Container.js
+++ src/elements/Container.js
@@ -1,11 +1,12 @@
-
+import Situation from "./Situation";
 import "../styles/situation.css";
 
 const Container = () => {
+
   return (
       <>
           <div className="container" id="cont">
-
+              <Situation/>
           </div>
       </>
   )
